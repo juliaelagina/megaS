a9fdbf5e07dc5bc2f11762f138359bb8ba097d52
juliaelagina <61758700+juliaelagina@users.noreply.github.com>
2020-07-24 20:27:29 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,30 @@
+
+
+
+$('.arrow-left').hover(
+    function () {
+        $('.arrow-left').html('<img src="img/оранжстрелка.png">');
+    },
+    function(){
+        $('.arrow-left').html('<img src="img/Фигура_1565_копия.png">');
+    });
+
+    
+$('.arrow-right').hover(
+    function () {
+        $('.arrow-right').html('<img src="img/оранжстрелка_вправо.png">');
+    },
+    function(){
+        $('.arrow-right').html('<img src="img/Фигура_1565_left.png">');
+    });
+
+    $('.allproducts').hover(
+        function () {
+            $('.allproducts').html('Все товары<img src="img/Фигура_16оранж.png">');
+        },
+        function(){
+            $('.allproducts').html('Все товары<img src="img/стрелка_в_кнопке.png">');
+        });
+        
+
+
